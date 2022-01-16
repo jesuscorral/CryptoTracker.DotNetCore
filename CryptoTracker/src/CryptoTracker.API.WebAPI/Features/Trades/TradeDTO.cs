0eeb91510816fdff26faabbb5f8d3278619512da
Jesús <corralperez@gmail.com>
2022-01-16 13:59:35 +0100

    Initial structure

---
+++ CryptoTracker/src/CryptoTracker.API.WebAPI/Features/Trades/TradeDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoTracker.API.WebAPI.Features.Trades
+{
+    public class TradeDTO
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ CryptoTracker/src/CryptoTracker.API.WebAPI/Features/Trades/TradesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CryptoTracker.API.WebAPI.Features.Trades
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TradesController
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TradesController> _logger;
+
+        public TradesController(ILogger<TradesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTrades")]
+        public IEnumerable<TradeDTO> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new TradeDTO
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
+
